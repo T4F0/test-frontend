059d9ee94c545464469ae4bce2d22a55d29f5573
t4f0 <selloummoncif.5@gmail.com>
2026-04-03 16:55:10 +0100

    change base URL to deployed backend

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,33 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:8000',
+    //     changeOrigin: true
+    //   },
+    //   '/ws': {
+    //     target: 'ws://localhost:8000',
+    //     ws: true,
+    //     changeOrigin: true
+    //   },
+    //   '/media': {
+    //     target: 'http://localhost:8000',
+    //     changeOrigin: true
+    //   }
+    // }
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://app.alpha.openscaler.net:9077',
         changeOrigin: true
       },
       '/ws': {
-        target: 'ws://localhost:8000',
+        target: 'ws://app.alpha.openscaler.net:9077',
         ws: true,
         changeOrigin: true
       },
       '/media': {
-        target: 'http://localhost:8000',
+        target: 'http://app.alpha.openscaler.net:9077',
         changeOrigin: true
       }
     }
